3132a556fd496c291328173b131ce4fa88de97e0
CTDictator <vladyslavnepo@gmail.com>
2023-09-11 19:02:32 -0400

    Interact visual on counters added.
    Player can walk over to an empty counter and visually see it is within range of interaction. Player can press the interact button and an interaction can happen.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,139 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { get; private set; }
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs: EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private float movementSpeed;
     [SerializeField] private PlayerController controller;
+    [SerializeField] private LayerMask countersLayerMask;
     private bool isWalking;
+    private Vector3 lastInteractDirection;
+    private ClearCounter selectedCounter;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("More than one instance of player present.");
+        }
+        else Instance = this;
+    }
+
+    private void Start()
+    {
+        controller.OnInteractAction += Controller_OnInteractAction;
+    }
+
+    private void Controller_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if (selectedCounter != null)
+        {
+            selectedCounter.Interact();
+        }
+    }
 
     private void Update()
     {
-        Vector2 inputVector = controller.GetMovementVectorNormalized();
-        Vector3 moveDirection = new(inputVector.x, 0.0f, inputVector.y);
-        transform.position += movementSpeed * Time.deltaTime * moveDirection;
-        isWalking = moveDirection != Vector3.zero;
-        float rotationSpeed = 10.0f;
-        transform.forward = Vector3.Slerp(transform.forward,
-            moveDirection, Time.deltaTime * rotationSpeed);
-        Debug.Log(inputVector);
+        HandleMovement();
+        HandleInteractions();
     }
 
     public bool IsWalking()
     {
         return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = controller.GetMovementVectorNormalized();
+        Vector3 moveDirection = new(inputVector.x, 0.0f, inputVector.y);
+        if (moveDirection != Vector3.zero)
+        {
+            lastInteractDirection = moveDirection;
+        }
+        float interactDistance = 2.0f;
+        if (Physics.Raycast(transform.position, lastInteractDirection, 
+            out RaycastHit hit, interactDistance, countersLayerMask))
+        {
+            if (hit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has a clear counter component.
+                if (clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
+            }
+            else
+            {
+                SetSelectedCounter(null);
+            }
+        }
+        else
+        {
+            SetSelectedCounter(null);
+        }
+    }
+
+    private void HandleMovement()
+    {
+        Vector2 inputVector = controller.GetMovementVectorNormalized();
+        Vector3 moveDirection = new(inputVector.x, 0.0f, inputVector.y);
+
+        float moveDistance = movementSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2.0f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position
+            + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (!canMove)
+        {
+            // Can not move towards the movement direction.
+            // Try X Movement.
+            Vector3 moveDirX = new Vector3(moveDirection.x, 0.0f, 0.0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position
+                + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if (canMove)
+            {
+                // Can only move on the X.
+                moveDirection = moveDirX;
+            }
+            else
+            {
+                // Can't move on X.
+                // Try Z movement Instead.
+                Vector3 moveDirZ = new Vector3(0.0f, 0.0f, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position
+                    + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                if (canMove)
+                {
+                    // Can move on the Z.
+                    moveDirection = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove)
+        {
+            transform.position += moveDistance * moveDirection;
+        }
+
+        isWalking = moveDirection != Vector3.zero;
+        float rotationSpeed = 10.0f;
+        transform.forward = Vector3.Slerp(transform.forward,
+            moveDirection, Time.deltaTime * rotationSpeed);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs 
+            { selectedCounter = this.selectedCounter });
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,22 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    PlayerInputActions inputActions;
+    public event EventHandler OnInteractAction;
+    private PlayerInputActions inputActions;
     private void Awake()
     {
         inputActions = new();
         inputActions.Player.Enable();
+        inputActions.Player.Interact.performed += Interact_performed;
     }
+
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
